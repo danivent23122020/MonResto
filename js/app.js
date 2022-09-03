8942138015ceca091223753e936a93dd334d505a
danivent23122020 <dani.vent@orange.fr>
2022-09-03 11:06:39 +0200

    modif

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@ window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+//
+function toggleMenu() {
+    const toggleMenu = document.querySelector(".menutoggle");
+    const navbar = document.querySelector(".navbar");
+    toggleMenu.classList.toggle("active");
+    navbar.classList.toggle("active");
+}
